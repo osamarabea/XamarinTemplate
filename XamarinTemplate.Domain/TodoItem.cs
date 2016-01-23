042d4588334a194b624a0d40a14548f9dceed1f4
Jacob Sønderskov <jacobsonderskov@gmail.com>
2016-01-23 09:35:31 +0100

    Initial commit.

---
+++ XamarinTemplate.Domain/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace XamarinTemplate.Domain
+{
+	public class TodoItem
+	{
+		public TodoItem ()
+		{
+		}
+
+		public int Id { get; set; }
+		public string Text { get; set; }
+
+	}
+}
+
---
+++ XamarinTemplate.iOS/Views/MainView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+using Cirrious.MvvmCross.Touch.Views;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using XamarinTemplate.Core.ViewModels;
+using System.Linq.Expressions;
+using Accelerate;
+
+namespace XamarinTemplate.iOS
+{
+	public partial class MainView : MvxViewController
+	{
+		public MainView() : base ("MainView", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad ();
+
+			var bindings = this.CreateBindingSet<MainView, MainViewModel> (); 
+			bindings.Bind(ViewModelLabel).For(label => label.Text).To(vm => vm.Hello); 
+			bindings.Apply();
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
